cee930d3513ef64f061c77c8d1241dfb80e28158
AJVR <anavelezrueda@gmail.com>
2020-10-06 19:19:49 -0300

    carousel init

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-fetch('https://api.giphy.com/v1/gifs/search?api_key=Nc8u10QS9qz9vLVNpc7W08yiQVxITRYJ&q=hamburguer').then(it => it.json().then(it => console.log(it)))
+//fetch('https://api.giphy.com/v1/gifs/search?api_key=Nc8u10QS9qz9vLVNpc7W08yiQVxITRYJ&q=hamburguer').then(it => it.json().then(it => console.log(it))) ..//
+
+
