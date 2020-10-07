b78a72db8030ef850e79e1d898b7093440de7e68
AJVR <anavelezrueda@gmail.com>
2020-10-06 21:04:40 -0300

    carousel js scrit for foward button

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 //fetch('https://api.giphy.com/v1/gifs/search?api_key=Nc8u10QS9qz9vLVNpc7W08yiQVxITRYJ&q=hamburguer').then(it => it.json().then(it => console.log(it))) ..//
 
+let carouselOffset = 0;
 
+
+function nextCarouselImage() {
+	carouselOffset += 3;	
+
+	const listaGifs = Array.from(document.getElementsByClassName('gif'));
+
+	listaGifs.forEach((elemento, indice) => {
+		if (indice < carouselOffset || indice >= carouselOffset +3) {
+			elemento.classList.remove('active');
+		} else {
+			elemento.classList.add('active');
+		}
+	})
+}
+
+
+document.getElementById('avanzar').addEventListener('click', nextCarouselImage)
