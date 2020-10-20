bc5e37d157dad502429cd60e483e06216dfedba8
Ana <anavelezrueda@gmail.com>
2020-10-20 18:06:15 -0300

    dynamic results div creator

--- general_functions.js
+++ general_functions.js
@@ -6,24 +6,51 @@ function iconsUnhover(elemento) {
     elemento.classList.remove('icons-unhover');
 }
 
+function creatImgObjetc(imgSrc, imgClassName, imgAltName) {
+    const img = document.createElement('img');
+    img.className = imgClassName;
+    img.alt = imgAltName;
+    img.src = imgSrc;
+    return img
+}
+
+
+function createOverlayDiv() {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.className = "overlay";
+    return overlayDiv
+}
+
 function renderGifos(gifosList, parent) {
     gifosList.forEach((elemento) => {
         const figure = document.createElement('figure');
+        const overlayDiv = createOverlayDiv();
+        const gifoImg = creatImgObjetc(elemento.src, 'result', elemento.alt);
+        const favImg = creatImgObjetc("images/icon-fav.svg", 'fav-icon', "fav");
+        const favImgHover = creatImgObjetc("images/icon-fav-hover.svg", "fav-icon icon-unhover", "fav-hover");
+        const maxImg = creatImgObjetc("images/icon-max-normal.svg", "max-button", "icon-max");
+        const maxImgHover = creatImgObjetc("images/icon-max-hover.svg", "max-button icon-unhover", "icon-max-hover");
+        const downloadImg = creatImgObjetc("images/icon-download.svg", "download-button", "download-button");
+        const downloadImgHover = creatImgObjetc("images/icon-download-hover.svg", "download-button icon-unhover", "download-button-hover");
+
         figure.setAttribute("class", "gifo");
-        figure.innerHTML = `<img src="${elemento.src}" class="result" alt="${elemento.alt}">
-        <div class="overlay">
-        <img src="images/icon-fav.svg" alt="fav" class="fav-icon">
-        <img src="images/icon-fav-hover.svg" alt="fav" class="fav-icon icon-unhover">
-        <img src="images/icon-max-normal.svg" alt="icon-max" class="max-button">
-        <img src="images/icon-max-hover.svg" alt="icon-max" class="max-button icon-unhover">
-        <img src="images/icon-download-hover.svg" alt="icon-download" class="download-button">
-        <img src="images/icon-download.svg" alt="icon-download" class="download-button icon-unhover">
-        </div>`;
-        parent.appendChild(figure);
+        figure.appendChild(gifoImg);
+
+        overlayDiv.appendChild(favImg);
+        overlayDiv.appendChild(favImgHover);
+        overlayDiv.appendChild(maxImg);
+        overlayDiv.appendChild(maxImgHover);
+        overlayDiv.appendChild(downloadImg);
+        overlayDiv.appendChild(downloadImgHover);
+        figure.appendChild(overlayDiv);
+
         favIcon = Array.from(figure.getElementsByClassName("fav-icon"))[0];
+
         favIcon.addEventListener('click', () => {
             saveFavs(elemento);
         });
+
+        parent.appendChild(figure);
     })
 }
 
