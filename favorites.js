d1086ca418ef91482b2b8733b1d76b15b55b2db8
AJVelezRueda <anavelezrueda@gmail.com>
2020-10-26 20:45:05 -0300

    ver mas changes

--- favorites.js
+++ favorites.js
@@ -1,3 +1,7 @@
 const favorites = getFavorites();
+const listaElementosNocturnos = ['body', 'ver-mas', 'compartir-text', 'footer-text']
+
 
 renderGifos(favorites, document.getElementById("search-results"), true);
+
+document.getElementById('boton-nocturno').addEventListener('click', () => modoNocturno(listaElementosNocturnos));
